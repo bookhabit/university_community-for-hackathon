87456762311f698211fa8dd3a7f53450edc52c4d
bookhabit <qnrgoqlt@naver.com>
2022-08-08 17:57:09 +0900

    게시글 목록에서 게시글 클릭하면 url로 index 넘겨줘서 해당index 게시글 보여주기(showPost)

--- board/js/major.js
+++ board/js/major.js
@@ -1,10 +1,16 @@
 // 페이지 이동
-function goShowPost() {
-  location.href = "ShowPost.html";
-}
 function goWritePost() {
   location.href = "WritePost.html";
 }
+
+// li onclick으로 이벤트 객체 받은 문자열앞자리로 index를 넘겨주기
+const showPost = (e) => {
+  // console.log(e.target.innerText[0]);
+  const POSTINDEX = e.target.innerText[0] - 1; //[0] 인덱스는 첫번째 문자를 뜻하고 화면에 출력할 때 +1을 해줬기 떄문에 실제데이터에 접근할 때는 -1을 해줘야함
+  console.log(POSTINDEX);
+  location.href = "showPost.html" + POSTINDEX;
+};
+
 // localStorage에 저장되어있는 게시글 배열가져와서 게시글 목록 렌더링
 // getItem localStoarge에 저장한 데이터를 가져오기
 const PostListString = window.localStorage.getItem("postList");
@@ -29,9 +35,9 @@ console.log(ul);
 // 텍스트 노드와 li를 PostList에 있는 만큼 생성
 for (var i = 0; i < PostList.length; i++) {
   var li = document.createElement("li");
-  var newPostTitle = document.createTextNode(PostList[i].title);
+  li.addEventListener("click", showPost);
+  var newPostTitle = document.createTextNode(`${i + 1}. ${PostList[i].title}`);
   li.appendChild(newPostTitle);
-
   ul.appendChild(li);
 }
 
--- board/js/showPost.js
+++ board/js/showPost.js
@@ -3,27 +3,40 @@ const PostListString = window.localStorage.getItem("postList");
 
 // 가져온 JSON 문자열을 객체로 변환
 const PostList = JSON.parse(PostListString);
-console.log(PostList);
-console.log(PostList[0].comment.length);
+// console.log(PostList);
+// console.log(PostList[0].comment.length);
 
-// 클릭된 게시글의 index를 가져오는 방법 필요
+// 클릭된 게시글의 index 가져와서 데이터배열의 index에넣어주기
+// location.href  에서 URL과 함께 보낸 데이터 받기
+// 정규식 표현을 이용한 함수
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results == null
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+var POSTINDEX = getParameterByName("type"); // 1060192
+console.log("POSTINDEX = " + POSTINDEX);
 
 // 제목, 본문, 댓글에  객체형식으로 innerText로 넣어보기
 const title = document.querySelector(".BoardDetail .title");
 const content = document.querySelector(".BoardDetail .content");
 const replComment = document.querySelector(".replDiv .comment");
 
-title.innerHTML = PostList[0].title; // 제목
-content.innerHTML = PostList[0].content; // 본문
+title.innerHTML = PostList[POSTINDEX].title; // 제목
+content.innerHTML = PostList[POSTINDEX].content; // 본문
 
 // 부모요소 만들기   - 저장되어 있는 데이터로 DOM요소에 추가해서 화면에 출력
 
 // 댓글만 새로 생성하는 식으로 하면 됨
 // 여기서 0은 클릭된 게시글의 객체하나만 보여주기 때문에 PostList의 인덱스값이 정해져있다
-if (PostList[0].comment.length > 0) {
-  for (var i = 0; i < PostList[0].comment.length; i++) {
+if (PostList[POSTINDEX].comment.length > 0) {
+  // 댓글이 1개라도 있다면 출력
+  for (var i = 0; i < PostList[POSTINDEX].comment.length; i++) {
     const p = document.createElement("p");
-    const pText = document.createTextNode(PostList[0].comment[i].text);
+    const pText = document.createTextNode(PostList[POSTINDEX].comment[i].text);
     p.appendChild(pText);
     replComment.appendChild(p);
   }
