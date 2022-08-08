b0d9454c2caf8457df39de5c98cd3162b1692c3a
bookhabit <qnrgoqlt@naver.com>
2022-08-08 20:35:18 +0900

    Posting

--- board/js/writePost.js
+++ board/js/writePost.js
@@ -1,9 +1,17 @@
+// getItem localStoarge에 저장한 데이터를 가져오기
+const PostListString = window.localStorage.getItem("postList");
+
+// 가져온 JSON 문자열을 객체로 변환
+const PostList = JSON.parse(PostListString);
+console.log(PostList);
+
 // DOM 요소 접근
 const inputForm = document.querySelector("#inputForm"); // form요소
 const inputTitle = document.querySelector("#inputForm #title"); // input title
 console.log(inputTitle);
-const inputContent = document.getElementById("#inputForm textarea");
+const inputContent = document.querySelector("#inputForm #content");
 console.log(inputContent);
+
 // submit 기본기능 제어 + 사용자 입력값 출력
 inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,20 +19,28 @@ inputForm.addEventListener("submit", (event) => {
   console.log(PostTitle);
   const PostContent = inputContent.value;
   console.log(PostContent);
+
+  // 제목 본문 입력값 받아서 객체로 저장하기  - 게시글 객체 형식
+  // 객체 하나 추가할 때 id값 +1해주기  삭제할 때 id값 -1 해주기
+  var postNum = tempPostList.length;
+
+  const EachPost = {
+    id: postNum + 1, // 게시글 하나당 고유한 id값을 가짐
+    title: inputTitle,
+    content: PostContent,
+    comment: [],
+    commentCount: 0,
+    like: Boolean,
+    saved: Boolean,
+    userName: "",
+  };
+
+  // 객체를 JSON문자열로 변환
+  const EachPostString = JSON.stringify(EachPost);
+
+  // 게시글 한개인 객체하나를 게시글 데이터베이스에 저장하기
+  window.localStorage.setItem("postList", EachPost);
 });
-
-// 제목 본문 입력값 받아서 객체로 저장하기
-// 게시글 객체 형식
-// 객체 하나 추가할 때 id값 +1해주기  삭제할 때 id값 -1 해주기
-const EachPost = {
-  id: 1,
-  title: "",
-  content: "",
-  comment: [],
-  commentCount: 0,
-  like: Boolean,
-  saved: Boolean,
-  userName: "",
-};
-
-// 게시글 한개인 객체하나를 게시글 데이터베이스에 저장하기
+PostTitle = "";
+PostContent = "";
+console.log(PostList);
